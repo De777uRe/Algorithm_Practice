ed60d28c96f1cc0bf6b34c2c4922b864609adc65
wgoins6 <wgoins6@gatech.edu>
2020-08-16 22:37:00 -0500

    Fixed previous implementation not utilizing dictionary

--- my_adjacent_nums.py
+++ my_adjacent_nums.py
@@ -5,26 +5,22 @@ def adjacent_nums(n):
     output_list = []
     valid_nums = {1: [i for i in range(10)]}
 
-    for num in range(n+1):
+    for num in range(n + 1):
         if num in valid_nums[1]:
             output_list.append(num)
         else:
-            num_copy = num
-            num_copy_string = str(num_copy)
-            is_valid = True
-            while len(str(num_copy)) > max(valid_nums.keys()):
-                if abs(int(num_copy_string[-1]) - int(num_copy_string[-2])) == 1:
-                    num_copy //= 10
-                    num_copy_string = str(num_copy)
-                else:
-                    is_valid = False
-                    break
-            if num_copy in [number for sublist in valid_nums.values() for number in sublist]:
+            num_copy_string = str(num)
+            if len(num_copy_string) > max(valid_nums.keys()):
+                if len(str(num)) not in valid_nums:
+                    valid_nums[len(str(num))] = []
+            if abs(int(num_copy_string[-1]) - int(num_copy_string[-2])) == 1 and \
+                    int(num_copy_string[:-1]) in [number for sublist in valid_nums.values()
+                                                  for number in sublist]:
+                valid_nums[len(str(num))].append(num)
                 output_list.append(num)
 
     return output_list
 
 
 if __name__ == "__main__":
-    print(adjacent_nums(10000))
-
+    print(adjacent_nums(100000))
